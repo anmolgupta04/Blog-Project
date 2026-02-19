0922b7cb06ea1afac563060bec6eb4c1d9b6b08b
natsu8411 <anmolgupta0415@gmail.com>
2026-02-19 22:36:17 +0530

    feat: add LOGIN_REDIRECT_URL to redirect users to home page after login

--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
 
+# Redirect users after login to site root (home page)
+LOGIN_REDIRECT_URL = '/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
